861bfa181971e2b5c32cc1e3ce1d65de913f84c7
Camila Modena <itscamilla@hotmail.com>
2022-02-14 17:12:03 +0200

    Generated new rails app

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+# As a user I can...
+
+# See all found pets
+# See details about one pet
+# Add a pet that I found
+# Update details of a pet
+# Delete a pet
+
+# POST - Create
+# GET - Read
+# PATCH - Update
+# DELETE - Delete
